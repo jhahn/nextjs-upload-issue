e23cbebbcf8a47d3f804926edb13c4372c2db78d
Jorgen Hahn <jhahn@niveon.com>
2022-08-03 16:01:24 -0400

    Add additional issue reproduction case

---
+++ pages/large-post.tsx
@@ -0,0 +1,17 @@
+/** Add your relevant code here for the issue to reproduce */
+export default function Home() {
+    const largePost = async (files: FileList | null) => {
+        const formData = new FormData()
+        const succeeds = new Uint8Array(16383);
+        const fails = new Uint8Array(16384);
+        self.crypto.getRandomValues(succeeds);
+        self.crypto.getRandomValues(fails);
+        const response = await fetch("/api/hello", {
+            method: "POST",
+            body: fails,
+        })
+        console.log(await response.json())
+    }
+
+    return <button onClick={largePost}>Send Large Post</button>
+}
